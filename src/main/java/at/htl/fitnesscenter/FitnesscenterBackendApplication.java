b428fe7eed9a65825196a5db5763f72873c5cdce
Michii <enes.kar.student@htl-hallein.at>
2025-11-25 09:44:52 +0100

    Initial commit

---
+++ src/main/java/at/htl/fitnesscenter/FitnesscenterBackendApplication.java
@@ -0,0 +1,13 @@
+package at.htl.fitnesscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesscenterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnesscenterBackendApplication.class, args);
+    }
+
+}
